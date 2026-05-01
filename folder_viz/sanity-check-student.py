71cb6ee327936eb3e01d7c61e9599ce5ad43f873
Amineki6 <kinaamine@gmail.com>
2026-05-01 02:39:06 +0200

    refactor: simplify sampling initializers by removing y_mode and unused x_mode priors, and refactor student model training scripts.

--- folder_viz/sanity-check-student.py
+++ folder_viz/sanity-check-student.py
@@ -10,7 +10,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 from folder_first_stage.autoencoder import Autoencoder
-from folder_third_stage.train_student import StudentAutoencoder
+from folder_third_stage.models import StudentAutoencoder
 
 def visualize_comparison(num_samples=6, student_weights='weights/student_autoencoder.pth'):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
